279735fac0270724def3c97c7068d7d7a72942a8
Park52 <wogus8660@gmail.com>
2019-05-29 13:39:39 +0900

    42586 Function Develop

---
+++ 42586/42586.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+	vector<int> answer;
+	queue<int> Queue;
+
+	for (int i = 0; i < progresses.size(); i++)
+	{
+		Queue.push((100 - progresses[i]) / speeds[i]);
+	}
+
+	int Pivot = Queue.front();
+	Queue.pop();
+	int Count = 1;
+
+	while (!Queue.empty())
+	{
+		if (Pivot >= Queue.front())
+		{
+			Count++;
+		}
+		else
+		{
+			answer.push_back(Count);
+			Pivot = Queue.front();
+			Count = 1;
+		}
+		Queue.pop();
+	}
+	answer.push_back(Count);
+	return answer;
+}
